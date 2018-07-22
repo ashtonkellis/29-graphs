595f3f47f43accbece757be1aa748c42e004a299
Ashton K Ellis <ashtonkellis@gmail.com>
2018-07-21 18:41:43 -0700

    scaffolding done

---
+++ src/lib/graph.js
@@ -0,0 +1,12 @@
+// class Node {
+//   constructor(value) {
+//     this.value = value;
+//     this.neighbors = [];
+//   }
+// }
+
+// export default class Graph {
+//   constructor() {
+
+//   }
+// }
